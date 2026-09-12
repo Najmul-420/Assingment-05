e2c56742bdf80ee0cef309f8816ff3dc3291d019
Najmul-420 <nh2117706@gmail.com>
2026-09-12 13:50:14 +0600

    second-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import { Suspense } from "react";
-// import Banner from "./components/Banner";
+
 
 import Navber from "./components/Navber";
 
 
-import Technologys from "./technology/technologys";
-
-// import type { Itechnologys } from "./type/technologys";
 import { ToastContainer } from "react-toastify";  
 
-// import Footer from './components/Footer/Footer'
-
-
-
-const technologysFetch = async (): Promise<Itechnologys[]> => {
-  const res = await fetch("/data.json");
-
-  const data = await res.json();
-  return data;
-
-};
-
-
-//  const technologysPromise = technologysFetch();
 
 function App() {
  
@@ -31,17 +14,17 @@ function App() {
     <> 
       <Navber />
       
-      {/* <Banner /> */}
+    
 
       <Suspense fallback={<div className="text-center font-bold text-2xl">Loading...</div>}>
         
-        {/* <Technologys technologysPromise={technologysPromise} /> */}
+        
 
       </Suspense>
    
     <ToastContainer />
 
-    {/* <Footer /> */}
+    
    
     </>
 
